17fdc69965ab73acc321b99fe55367597ada44e6
Tarin0 <tarinmostofa837@gmail.com>
2024-03-29 16:14:20 +0600

    delete api created for cart

--- index.js
+++ index.js
@@ -47,13 +47,19 @@ async function run() {
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
-    
+
     app.post('/carts', async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     });
 
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
