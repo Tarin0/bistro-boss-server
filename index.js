34559e445a1c6f8acc2423c4f29a64f83826582c
Tarin0 <tarinmostofa837@gmail.com>
2024-03-29 15:41:48 +0600

    get cart added

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
-    
+
     app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
         res.send(result);
@@ -40,6 +40,14 @@ async function run() {
         res.send(result);
     })
 
+    // carts collection
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    
     app.post('/carts', async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
